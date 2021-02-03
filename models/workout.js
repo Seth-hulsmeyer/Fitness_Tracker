8eddf8e9a296c668210b93510458e4a4402925e6
Seth-hulsmeyer <seth.hulsmeyer@hotmail.com>
2021-02-02 20:19:30 -0700

    app mostly working, getting an error when trying to display stats page

--- models/workout.js
+++ models/workout.js
@@ -2,35 +2,38 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const opts = { toJSON: { virtuals: true } };
+const virtual = { toJSON: { virtuals: true } };
 //Schema goes here
 
-const sessionSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now,
-  },
-  exercise: [
-    {
-      type: {
-        type: String,
-        required: "You must enter the type of the workout",
-      },
-      name: {
-        type: String,
-        required: "You must enter in the name of the workout",
-      },
-      duration: {
-        type: Number,
-        required: "Your workout must have a duration",
-      },
-      weight: Number,
-      reps: Number,
-      sets: Number,
-      distance: Number,
+const sessionSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-  ],
-});
+    exercise: [
+      {
+        type: {
+          type: String,
+          required: "You must enter the type of the workout",
+        },
+        name: {
+          type: String,
+          required: "You must enter in the name of the workout",
+        },
+        duration: {
+          type: Number,
+          required: "Your workout must have a duration",
+        },
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number,
+      },
+    ],
+  },
+  virtual
+);
 
 sessionSchema.virtual("totalDuration").get(function () {
   return this.exercise.reduce((total, exercise) => {
