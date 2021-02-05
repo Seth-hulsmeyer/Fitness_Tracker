54f0e8e30e419ed6ffa1ebe40b087860c94767e7
Seth-hulsmeyer <seth.hulsmeyer@hotmail.com>
2021-02-04 21:36:54 -0700

    correct .connect error

--- server.js
+++ server.js
@@ -13,15 +13,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //mongoose middleware
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(require("./routes/html-routes"));
 app.use(require("./routes/api-routes"));
